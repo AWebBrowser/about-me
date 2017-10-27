d6a5d376cc2de13c347a33c6030d14d3cd55384d
AWebBrowser <gabrielito0604@gmail.com>
2017-10-26 14:38:59 -1000

    Finshed with aboutme.js , will no longer update :(

--- aboutme.js
+++ aboutme.js
@@ -1,12 +1,12 @@
 // Create sanity check below
 
-
+console.log("Hello World");
 
 // console.log each variable
 // follow example below (remove "//" to uncomment)
 
-// var school = "Kapolei Middle School";
-// console.log(school); <== this should print "Kapolei Middle School" in your browser
+var school = "Kapolei Middle School";
+console.log(school); //<== this should print "Kapolei Middle School" in your browser
 
 
 // # 1
@@ -23,11 +23,17 @@
 // favSport
 // favDoughnut
 
-
-
-
-
-
+var firstName = "Jan";
+var lastName = "Armendariz-Bones";
+var birthPlace = "Fort Drum, New York, United States, Earth, Milky Way, Universe #1";
+var favFood = "Pizza"
+var favDrink = "Coke II (2) (Discountinued in 70's)";
+var favSong = "March From The Nutcracker";
+var favAnimal = "Dinosaur";
+var favColor = "Very Very Bright Blue";
+var favSport = "Rugby";
+var favDoughtnut = "Chocolate Glazed Doughnut";
+// End #1
 
 // #2
 // Variables with a Number value
@@ -39,10 +45,12 @@
 // numberOfStudentsInClass
 // numberOfDoorsInClassroom
 
-
-
-
-
+var favNumber = 9223372036854775807;
+var yourShoeSize = 9.5;
+var numberOfPets = 0;
+var numberOfSiblings = 1;
+var numberOfStudentsInClass = 26;
+var numberOfDoorsInClassroom = 2;
 
 // #3
 // Variables with a Boolean value
@@ -54,10 +62,12 @@
 // playThePiano
 // playSports
 
-
-
-
-
+likesMcDonalds = true;
+eatsDoughnuts = true;
+ownsRedShoes = false;
+hasAPet = false;
+playThePiano = true;
+playSports = false;
 
 // #4
 // Variables with a Null value
@@ -66,10 +76,9 @@
 // traveledToMars
 // ateDinner
 
-
-
-
-
+var completedMiddleSchool = null;
+var traveledToMars = null;
+var ateDinner = null;
 
 // #5
 // Variables with an undefined value
@@ -78,10 +87,9 @@
 // lastDayOfSchool
 // nextFieldTrip
 
-
-
-
-
+var superBowlChamps;
+var lastDayOfSchool;
+var nextFieldTrip;
 
 // #6
 // Variables with an Array value
@@ -89,3 +97,86 @@
 // Declare a variable named plateLunch and assign it an array containing 5 of your favorite lunch items.
 // Declare a variable named `donutBox` and assign it an array containing 5 donuts of your choosing.
 // Declare a variable named `pizzaToppings` and assign it an array containing 5 pizza toppings of your choosing.
+
+var backpack = ["Phone","AVID Binder", "Band Binder", "Pencils", "Homework"];
+var plateLunch = ["Pizza", "Kalua Pork", "Sloppy Joe", "Hot Dog", "Hamburger"];
+var donutBox = ["Jelly Filled Dount","Pizza Donut","Glazed Dount","Strawberry Donut","Chocolate Glazed Donut"];
+var pizzaToppings = ["Peperoni","Sausage","Ham","Jalepeno","Cheese"];
+
+//Begin console Spam :(//
+
+//Start #1 logs//
+console.log(firstName);
+console.log(lastName);
+console.log(birthPlace);
+console.log(favFood);
+console.log(favDrink);
+console.log(favSong);
+console.log(favAnimal);
+console.log(favColor);
+console.log(favSport);
+console.log(favDoughtnut);
+//End #1 Logs//
+
+//Start #2 Logs//
+console.log(favNumber);
+console.log(yourShoeSize);
+console.log(numberOfPets);
+console.log(numberOfSiblings);
+console.log(numberOfStudentsInClass);
+console.log(numberOfDoorsInClassroom);
+//End #2 Logs//
+
+//Start #3 Logs//
+console.log(likesMcDonalds);
+console.log(eatsDoughnuts);
+console.log(ownsRedShoes);
+console.log(hasAPet);
+console.log(playThePiano);
+console.log(playSports);
+//End #3 Logs//
+
+//Begin #4 Logs//
+console.log(completedMiddleSchool);
+console.log(traveledToMars);
+console.log(ateDinner);
+//End #4 Logs//
+
+//Start #5 Logs//
+console.log(superBowlChamps);
+console.log(lastDayOfSchool);
+console.log(nextFieldTrip);
+//End #5 Logs//
+
+//Start #6 Logs//
+console.log(backpack);
+console.log(plateLunch);
+console.log(donutBox);
+console.log(pizzaToppings);
+//End #6 Logs//
+
+//Begin Final Log//
+console.log("I'm finally finished :D");
+//End Final Log//
+
+//End .js//
+
+//Extra Challenge #1//
+console.log(backpack[0])
+console.log(plateLunch[1])
+console.log(donutBox[2])
+console.log(pizzaToppings[3])
+
+//Extra Challenge #2//
+
+var person = {
+    firstName:"John",
+    lastName:"Doe",
+    age:50,
+    eyeColor:"blue"
+};
+console.log("objects test vvv");
+console.log(person.firstName);
+console.log(person.lastName)
+console.log(person.age);
+console.log(person.eyeColor)
